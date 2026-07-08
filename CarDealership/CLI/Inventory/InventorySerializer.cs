af534624d9feebca7a6962d7ccfb59f7f8756ef9
Ariel <ariel.levi.2007@gmail.com>
2026-07-08 19:23:25 +0300

    feat: serialize enums as strings

--- CarDealership/CLI/Inventory/InventorySerializer.cs
+++ CarDealership/CLI/Inventory/InventorySerializer.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace CarDealership.CLI.Inventory
@@ -11,7 +13,9 @@ namespace CarDealership.CLI.Inventory
     {
         private JsonSerializerOptions _options = new JsonSerializerOptions
         {
-            WriteIndented = true
+            WriteIndented = true,
+
+            Converters = { new JsonStringEnumConverter() }
         };
         //takes the object list and turns it into a json for later use
         public string ToJson(IReadOnlyList<object> items)
